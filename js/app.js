54648f01f64545d68f5c225e719c715f04a861dd
mdmehedihasan12 <mdmehedihasansohag0@gmail.com>
2024-09-25 00:59:47 +0600

    history added

--- js/app.js
+++ js/app.js
@@ -20,32 +20,37 @@ document
     event.preventDefault();
     const nInput = document.getElementById("n-input-add-money").value;
     const myBalance = document.getElementById("my-balance").innerText;
+    const nAmount = document.getElementById("n-amount").innerText;
     const myBalanceNumber = parseFloat(myBalance);
     const nInputNumber = parseFloat(nInput);
     const nAmountNumber = parseFloat(nAmount);
-    if (nInputNumber < myBalanceNumber || nInputNumber === "number") {
+    if (
+      nInputNumber > myBalanceNumber ||
+      typeof nInputNumber !== "number" ||
+      nInputNumber < 1
+    ) {
+      return alert("invalid Donation Amount");
+
+    } else if(isNaN(nInputNumber)){
+      return alert('Please provide a Number')
+    }
+    
+    else {
       const myNewBalance = myBalanceNumber - nInputNumber;
       const nAmountBalance = nAmountNumber + nInputNumber;
-
       document.getElementById("my-balance").innerText = myNewBalance;
       document.getElementById("n-amount").innerText = nAmountBalance;
-      
-      const h1 = document.createElement("h1");
-      h1.innerText = ` ${nInputNumber} Tk is Donated for famine-2024 at Noakhali, Bangladesh `;
-      
-      // const p = document.createElement('p')
-      // p.innerText = newDate();
-      document.getElementById("n-history").appendChild(h1);
-      
+      const div = document.createElement('div')
+      div.className =  'border py-6 px-5 rounded-xl mt-4'
+      div.innerHTML = `
+        <h1>${nInputNumber} Tk is Donated for famine-2024 at Noakhali, Bangladesh</h1>
+        <p>${new Date()}</p>
+      `
+      document.getElementById("history-form").appendChild(div);
 
-      
-    } else if (isNaN(nInputNumber)) {
-      alert("please provide a number");
-      return;
-    } else {
-      alert("you have no money");
-      return;
+      my_modal_1.showModal();
     }
+
   });
 // feni donation section
 
@@ -53,61 +58,74 @@ document.getElementById("f-button").addEventListener("click", function (event) {
   event.preventDefault();
   const fInput = document.getElementById("f-input").value;
   const myBalance = document.getElementById("my-balance").innerText;
+  const fAmount = document.getElementById("f-amount").innerText;
   const myBalanceNumber = parseFloat(myBalance);
   const fInputNumber = parseFloat(fInput);
   const fAmountNumber = parseFloat(fAmount);
 
-  if (fInputNumber < myBalanceNumber || fInputNumber === "number") {
+
+  if (
+    fInputNumber > myBalanceNumber ||
+    typeof fInputNumber !== "number" ||
+    fInputNumber < 1
+  ) {
+    return alert("invalid Donation Amount");
+
+  }else if(isNaN(fInputNumber)){
+    return alert('Please provide a Number')
+  }
+  
+  else {
     const myNewBalance = myBalanceNumber - fInputNumber;
     const fAmountBalance = fAmountNumber + fInputNumber;
-
     document.getElementById("my-balance").innerText = myNewBalance;
     document.getElementById("f-amount").innerText = fAmountBalance;
-
-    const h1 = document.createElement("h1");
-      h1.innerText = ` ${fInputNumber} Taka is Donated for Flood Relief in Feni , Bangladesh `;
-      
-      // const p = document.createElement('p')
-      // p.innerText = newDate();
-      document.getElementById("f-history").appendChild(h1);
-  } else if (isNaN(fInputNumber)) {
-    alert("please provide a number");
-    return;
-  } else {
-    alert("you have no money");
-    return;
+    const div = document.createElement('div')
+    div.className =  'border py-6 px-5 rounded-xl mt-4'
+    div.innerHTML = `
+      <h1>${fInputNumber} Tk is Donated for famine-2024 at Noakhali, Bangladesh</h1>
+      <p>${new Date()}</p>
+    `
+    document.getElementById("history-form").appendChild(div);
+    my_modal_1.showModal();
   }
+  
 });
 document.getElementById("q-button").addEventListener("click", function (event) {
   event.preventDefault();
   const qInput = document.getElementById("q-input").value;
   const myBalance = document.getElementById("my-balance").innerText;
+  const qAmount = document.getElementById("q-amount").innerText;
   const myBalanceNumber = parseFloat(myBalance);
   const qInputNumber = parseFloat(qInput);
   const qAmountNumber = parseFloat(qAmount);
 
-  if (qInputNumber < myBalanceNumber || qInputNumber === "number") {
+  if (
+    qInputNumber > myBalanceNumber ||
+    typeof qInputNumber !== "number" ||
+    qInputNumber < 1
+  ) {
+    return alert("invalid Donation Amount");
+  } else if(isNaN(qInputNumber)){
+    return alert('Please provide a Number')
+  }
+  
+  else {
     const myNewBalance = myBalanceNumber - qInputNumber;
     const qAmountBalance = qAmountNumber + qInputNumber;
 
     document.getElementById("my-balance").innerText = myNewBalance;
     document.getElementById("q-amount").innerText = qAmountBalance;
 
-    alert("your money added success");
-
-
-    const h1 = document.createElement("h1");
-      h1.innerText = ` ${qInputNumber} Tk is Donated for Aid for Injured in the quota Movement, Bangladesh `;
-      
-      // const p = document.createElement('p')
-      // p.innerText = newDate();
-      document.getElementById("q-history").appendChild(h1);
-  } else if (isNaN(qInputNumber)) {
-    alert("please provide a number");
-    return;
-  } else {
-    alert("you have no money");
-    return;
+    const div = document.createElement('div')
+    div.className =  'border py-6 px-5 rounded-xl mt-4'
+    div.innerHTML = `
+      <h1>${qInputNumber} Tk is Donated for famine-2024 at Noakhali, Bangladesh</h1>
+      <p>${new Date()}</p>
+    `
+    document.getElementById("history-form").appendChild(div);
+    my_modal_1.showModal();
+    
   }
 });
 
@@ -126,9 +144,11 @@ document
     document.getElementById("donation-btn").classList.add("bg-btnBg");
   });
 
-document.getElementById("history-btn").addEventListener("click", function (event) {
+document
+  .getElementById("history-btn")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-  showSectionById("history-form");
-  document.getElementById("donation-btn").classList.remove("bg-btnBg");
-  document.getElementById("history-btn").classList.add("bg-btnBg");
-});
+    showSectionById("history-form");
+    document.getElementById("donation-btn").classList.remove("bg-btnBg");
+    document.getElementById("history-btn").classList.add("bg-btnBg");
+  });
